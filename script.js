acb58e5aa60e9b9ab54c8a9b4656a5d53536b124
benzy78 <39614974+benzy78@users.noreply.github.com>
2018-06-12 23:15:31 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+$(function(){
+  $('.header-logo').click(function(){
+    $('html,body').animate({
+      'scrollTop':0
+    },'slow');
+  });
+
+  $('.scroll-language').click(function(){
+    var languageId=$(this).attr('href');
+    var languagePosition=$(languageId).offset().top;
+    $('html,body').animate({
+      'scrollTop':languagePosition
+    },'slow');
+  });
+
+  $('.scroll-feature').click(function(){
+    var feaId=$(this).attr('href');
+    var feaPosition=$(feaId).offset().top;
+    $('html,body').animate({
+      'scrollTop':feaPosition
+    },'slow');
+  });
+
+  $('.scroll-price').click(function(){
+    var priceId=$(this).attr('href');
+    var pricePosition=$(priceId).offset().top;
+    $('html,body').animate({
+      'scrollTop':pricePosition
+    },'slow');
+  });
+
+  $('.scroll-contact').click(function(){
+    var contactId=$(this).attr('href');
+    var contactPosition=$(contactId).offset().top;
+    $('html,body').animate({
+      'scrollTop':contactPosition
+    },'slow');
+  });
+
+  $('.top-description').fadeIn(4000);
+
+  $('.index-btn').click(function(){
+    $('.active').removeClass('active');
+    var clickIndex=$('.index-btn').index($(this));
+    $('.language-item').eq(clickIndex).addClass('active');
+  });
+
+  $('.header-right-iphone').hover(
+    function(){
+      $('.menu-list').fadeIn();
+    },
+    function(){
+      $('.menu-list').fadeOut();
+    }
+  );
+
+
+});
